d73b09116ae17cf016ff39208d1ff9746faa3a55
Murtaza Samim Choudhury <m.s.choudhury96@gmail.com>
2022-06-06 17:58:08 +0530

    Call Back Hell Task

---
+++ Tasks_Day1_Callback Hell Task/script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.querySelector('.container').innerHTML = `<h3>10</h3>`;
+    setTimeout(() => {
+        document.querySelector('.container').innerHTML = `<h3>9</h3>`;
+        setTimeout(() => {
+            document.querySelector('.container').innerHTML = `<h3>8</h3>`;
+            setTimeout(() => {
+                document.querySelector('.container').innerHTML = `<h3>7</h3>`;
+                setTimeout(() => {
+                    document.querySelector('.container').innerHTML = `<h3>6</h3>`;
+                    setTimeout(() => {
+                        document.querySelector('.container').innerHTML = `<h3>5</h3>`;
+                        setTimeout(() => {
+                            document.querySelector('.container').innerHTML = `<h3>4</h3>`;
+                            setTimeout(() => {
+                                document.querySelector('.container').innerHTML = `<h3>3</h3>`;
+                                setTimeout(() => {
+                                    document.querySelector('.container').innerHTML = `<h3>2</h3>`;
+                                    setTimeout(() => {
+                                        document.querySelector('.container').innerHTML = `<h3>1</h3>`;
+                                        setTimeout(() => {
+                                            document.querySelector('.container').innerHTML = `<h1 class="text-success">Happy Independence Day</h1>`;
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
